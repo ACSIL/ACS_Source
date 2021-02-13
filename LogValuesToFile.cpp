fcba2d85f1c2daf27cecc01fc81761f778d6ba61
lukas k <experimentalhypothesis@gmail.com>
2021-02-13 21:27:02 +0100

    logging market stats to file.csv

--- LogValuesToFile.cpp
+++ LogValuesToFile.cpp
@@ -35,39 +35,99 @@
 
 SCDLLName("Logger");
 
-void printToMessageLog(SCStudyInterfaceRef &sc, s_SCPositionData &position, SCFloatArray &study_1, SCFloatArray &study_2) {
-    int &p_logEntry = sc.GetPersistentInt(0);
+void printToMessageLog(SCStudyInterfaceRef &sc, s_SCPositionData &position, SCFloatArray &volume, SCFloatArray &atr, SCFloatArray &cumDelta) {
+    int &p_logEntryDone = sc.GetPersistentInt(0);
     int &p_previousPositionQty = sc.GetPersistentInt(1);
 
     bool positionClosed = position.PositionQuantity == 0;
-    bool positionOpened = p_previousPositionQty == 0 && position.PositionQuantity != 0;
-    bool logEntryNotYetDone = p_logEntry == 0;
+    bool newPositionOpened = p_previousPositionQty == 0 && position.PositionQuantity != 0;
+    bool logEntryNotYetDone = p_logEntryDone == 0;
 
-    float valueOfStudy_1 = study_1[study_1.GetArraySize() - 1];
-    float valueOfStudy_2 = study_1[study_2.GetArraySize() - 1];
+    float volumeOnPrevBar = volume[volume.GetArraySize() - 2];
+    float atrOnPrevBar = atr[atr.GetArraySize() - 2];
+    float cumDeltaOnPrevBar = cumDelta[cumDelta.GetArraySize() - 2];
 
     SCString stringToLog;
-    stringToLog.Format("Entry Price: %.02f, study_1: %.02f, study_2: %.02f", position.AveragePrice, valueOfStudy_1, valueOfStudy_2);
+    stringToLog.Format("Entry Price: %.02f, Volume: %.02f, ATR (exponencial, 7): %.02f, Cum Delta (from start of trading day) %.02f",
+                       position.AveragePrice, volumeOnPrevBar, atrOnPrevBar, cumDeltaOnPrevBar);
 
-    if (positionOpened && logEntryNotYetDone) {
+    if (newPositionOpened && logEntryNotYetDone) {
         sc.AddMessageToLog(stringToLog, 1);
-        p_logEntry = 1;
+        p_logEntryDone = 1;
     }
     if (positionClosed) {
-        p_logEntry = 0;
+        p_logEntryDone = 0;
     }
 }
 
-void printToCSVFile(SCStudyInterfaceRef &sc, s_SCPositionData position) {
-    return;
+void printToCSVFile(SCStudyInterfaceRef &sc, s_SCPositionData &position, SCFloatArray &volume, SCFloatArray &atr, SCFloatArray &cumDelta) {
+    int &p_logEntryDone = sc.GetPersistentInt(2);
+    int &p_previousPositionQty = sc.GetPersistentInt(3);
+
+    bool positionClosed = position.PositionQuantity == 0;
+    bool newPositionOpened = p_previousPositionQty == 0 && position.PositionQuantity != 0;
+    bool logEntryNotYetDone = p_logEntryDone == 0;
+
+    float volumeOnPrevBar = volume[volume.GetArraySize() - 2];
+    float atrOnPrevBar = atr[atr.GetArraySize() - 2];
+    float cumDeltaOnPrevBar = cumDelta[cumDelta.GetArraySize() - 2];
+
+    SCDateTime entryDateTime = position.LastEntryDateTime;
+    int year, month, day, hour, minute, second;
+    entryDateTime.GetDateTimeYMDHMS(year, month, day, hour, minute, second);
+
+    SCString stringToLog;
+    stringToLog.Format("Entry DateTime: %i-%i-%i %0.2i:%0.2i:%0.2i, Entry price: %3.2f, Symbol: %s, Quantity: %3.0f, Volume: %.02f, ATR (exponenecial(7)): %.02f, Cum. Delta (from session start): %.02f,",
+                       year, month, day, hour, minute, second,
+                       position.AveragePrice,
+                       position.Symbol,
+                       position.PositionQuantity,
+                       volumeOnPrevBar,
+                       atrOnPrevBar,
+                       cumDeltaOnPrevBar);
+
+    std::string fileName{"ES_log.csv"};  // variable pres input
+    std::ofstream outFile{};
+    outFile.open(fileName, std::ofstream::app);
+    if (newPositionOpened && logEntryNotYetDone) {
+        sc.AddMessageToLog("New Pos opened", 1);
+        if (outFile.is_open()) {
+            sc.AddMessageToLog("Opened file", 1);
+            std::string log{stringToLog};
+            outFile << log << '\n';
+        } else {
+            sc.AddMessageToLog("Error writing into file", 1);
+        }
+        outFile.close();
+    }
+    p_logEntryDone = 1;
+
+    if (positionClosed) {
+        p_logEntryDone = 0;
+    }
 }
+
+class Params {
+   public:
+    SCFloatArray volume;
+    SCFloatArray atr;
+    SCFloatArray cumDelta;
+
+    Params(SCFloatArray volume, SCFloatArray atr, SCFloatArray cumDelta) {
+        this->volume = volume;
+        this->atr = atr;
+        this->cumDelta = cumDelta;
+    }
+};
 
 SCSFExport scsf_LogEntryDetails(SCStudyInterfaceRef sc) {
     if (sc.SetDefaults) {
-        sc.Input[1].Name = "1. study";
-        sc.Input[2].Name = "2. study";
-        // sc.Input[3].Name = "3. study";
-        // sc.Input[4].Name = "4. study";
+        sc.Input[1].Name = "Volume";
+        sc.Input[1].SetChartStudySubgraphValues(0, 0, 0);
+        sc.Input[2].Name = "ATR";
+        sc.Input[2].SetChartStudySubgraphValues(0, 0, 0);
+        sc.Input[3].Name = "Cumulative delta";
+        sc.Input[3].SetChartStudySubgraphValues(0, 0, 0);
 
         sc.GraphRegion = 0;
         sc.GraphName = "Logger";
@@ -77,11 +137,15 @@ SCSFExport scsf_LogEntryDetails(SCStudyInterfaceRef sc) {
     s_SCPositionData position;
     sc.GetTradePosition(position);
 
-    SCFloatArray study_1;
-    SCFloatArray study_2;
-    sc.GetStudyArrayFromChartUsingID(sc.Input[1].GetChartNumber(), sc.Input[1].GetStudyID(), sc.Input[1].GetSubgraphIndex(), study_1);
-    sc.GetStudyArrayFromChartUsingID(sc.Input[2].GetChartNumber(), sc.Input[2].GetStudyID(), sc.Input[2].GetSubgraphIndex(), study_2);
+    SCFloatArray volume;
+    SCFloatArray atr;
+    SCFloatArray cumDelta;
+    sc.GetStudyArrayFromChartUsingID(sc.Input[1].GetChartNumber(), sc.Input[1].GetStudyID(), sc.Input[1].GetSubgraphIndex(), volume);
+    sc.GetStudyArrayFromChartUsingID(sc.Input[2].GetChartNumber(), sc.Input[2].GetStudyID(), sc.Input[2].GetSubgraphIndex(), atr);
+    sc.GetStudyArrayFromChartUsingID(sc.Input[2].GetChartNumber(), sc.Input[3].GetStudyID(), sc.Input[3].GetSubgraphIndex(), cumDelta);
 
-    printToMessageLog(sc, position, study_1, study_2);
-    // printToCSVFile(sc, position);
+    // params = new Params(volume, atr, cumDelta); // tohle potrebuju udelat perzistentni aby se to furt nevytvarelo a nenicilo
+
+    printToCSVFile(sc, position, volume, atr, cumDelta);
+    printToMessageLog(sc, position, volume, atr, cumDelta);
 }
--- ReadValuesAndLogIntoTxt.cpp
+++ ReadValuesAndLogIntoTxt.cpp
@@ -1,14 +1,13 @@
-#include "sierrachart.h"
 #include <fstream>
 #include <string>
 
+#include "sierrachart.h"
+
 SCDLLName("Logger")
 
-//dat do grafu kde vsechno vypisu 
-SCSFExport scsf_ReadValuesFromPerzistVarsAndLogIntoTXT(SCStudyInterfaceRef sc)
-{
-    if (sc.SetDefaults)
-    {
+    //dat do grafu kde vsechno vypisu
+    SCSFExport scsf_ReadValuesFromPerzistVarsAndLogIntoTXT(SCStudyInterfaceRef sc) {
+    if (sc.SetDefaults) {
         sc.Input[0].Name = "Chart for Tick";
         sc.Input[0].SetChartStudyValues(1, 0);
         sc.Input[1].Name = "Chart for ATR";
@@ -19,7 +18,7 @@ SCSFExport scsf_ReadValuesFromPerzistVarsAndLogIntoTXT(SCStudyInterfaceRef sc)
         sc.Input[3].SetChartStudyValues(4, 0);
         sc.Input[4].Name = "Chart for DOM Bid-Ask";
         sc.Input[4].SetChartStudyValues(5, 0);
-        sc.GraphRegion = 0;//same as main
+        sc.GraphRegion = 0;  //same as main
         sc.GraphName = "Read the perzists and log into txt";
         sc.FreeDLL = 1;
         sc.UpdateAlways = 1;
@@ -33,8 +32,10 @@ SCSFExport scsf_ReadValuesFromPerzistVarsAndLogIntoTXT(SCStudyInterfaceRef sc)
     int dom_quotes_ba = static_cast<int>(sc.GetStudyPersistentFloatFromChart(sc.Input[4].GetChartNumber(), sc.Input[4].GetStudyID(), 0));
 
     int dom{};
-    if (dom_quotes_ab > abs(dom_quotes_ba)) dom = dom_quotes_ab;
-    else dom = dom_quotes_ba;
+    if (dom_quotes_ab > abs(dom_quotes_ba))
+        dom = dom_quotes_ab;
+    else
+        dom = dom_quotes_ba;
 
     //set the internal sierra logs message details
 
@@ -43,67 +44,62 @@ SCSFExport scsf_ReadValuesFromPerzistVarsAndLogIntoTXT(SCStudyInterfaceRef sc)
     SCString log;
     s_SCPositionData PositionData;
     sc.GetTradePosition(PositionData);
-    std::string symbol{ PositionData.Symbol };
-    t_OrderQuantity32_64 qty{ PositionData.PositionQuantity };
+    std::string symbol{PositionData.Symbol};
+    t_OrderQuantity32_64 qty{PositionData.PositionQuantity};
 
     auto last_entry_dt = PositionData.LastEntryDateTime;
     int Year, Month, Day, Hour, Minute, Second;
     last_entry_dt.GetDateTimeYMDHMS(Year, Month, Day, Hour, Minute, Second);
-    log.Format("Entry DateTime: %i-%i-%i, %0.2i:%0.2i:%0.2i,  Entry price: %3.2f, Symbol: %s, Quantity: %3.0f, ATR: %3.2f, WTR: %i, DOM: %i", Year, Month, Day, Hour, Minute, Second, PositionData.AveragePrice, symbol, qty, atr_value, wtr_value, dom);
+    log.Format("Entry DateTime: %i-%i-%i, %0.2i:%0.2i:%0.2i,  
+               Entry price
+               : % 3.2f, Symbol
+               : % s, Quantity
+               : % 3.0f, ATR
+               : % 3.2f, WTR
+               : % i, DOM
+               : % i ", 
+                     Year,
+                 Month, Day, Hour, Minute, Second, PositionData.AveragePrice,
+                 symbol, qty, atr_value, wtr_value, dom);
 
-
-
-    if (previous_qt_perzist == 0 && PositionData.PositionQuantity != 0)
-    {
-        if (already_logged_perzist == 0)
-        {
-            if (symbol.at(0) == 'N' || symbol.at(5) == 'N') //log details for NQ entries into separate txt file
+    if (previous_qt_perzist == 0 && PositionData.PositionQuantity != 0) {
+        if (already_logged_perzist == 0) {
+            if (symbol.at(0) == 'N' || symbol.at(5) == 'N')  //log details for NQ entries into separate txt file
             {
-                std::string file_name{ "Entry logs NQ.txt" }; //custom foo crating string
+                std::string file_name{"Entry logs NQ.txt"};  //custom foo crating string
                 std::ofstream out_file{};
                 out_file.open(file_name, std::ofstream::app);
-                if (out_file.is_open())
-                {
-                    std::string test_logu{ log };
+                if (out_file.is_open()) {
+                    std::string test_logu{log};
                     out_file << test_logu << '\n';
-                }
-                else
-                {
-                    SCString log_error{ "Error writing into file" };
+                } else {
+                    SCString log_error{"Error writing into file"};
                     sc.AddMessageToLog(log_error, 1);
                 }
                 out_file.close();
-            }
-            else if (symbol.at(0) == 'Y' || symbol.at(5) == 'Y') //log details for YM entries into separate txt file
+            } else if (symbol.at(0) == 'Y' || symbol.at(5) == 'Y')  //log details for YM entries into separate txt file
             {
-                std::string file_name{ "Entry logs YM.txt" };
+                std::string file_name{"Entry logs YM.txt"};
                 std::ofstream out_file{};
                 out_file.open(file_name, std::ofstream::app);
-                if (out_file.is_open())
-                {
-                    std::string test_logu{ log };
+                if (out_file.is_open()) {
+                    std::string test_logu{log};
                     out_file << test_logu << '\n';
-                }
-                else
-                {
-                    SCString log_error{ "Error writing into file" };
+                } else {
+                    SCString log_error{"Error writing into file"};
                     sc.AddMessageToLog(log_error, 1);
                 }
                 out_file.close();
-            }
-            else if (symbol.at(0) == 'R' || symbol.at(5) == 'R') //log details for RTY entries into separate txt file
+            } else if (symbol.at(0) == 'R' || symbol.at(5) == 'R')  //log details for RTY entries into separate txt file
             {
-                std::string file_name{ "Entry logs RTY.txt" };
+                std::string file_name{"Entry logs RTY.txt"};
                 std::ofstream out_file{};
                 out_file.open(file_name, std::ofstream::app);
-                if (out_file.is_open())
-                {
-                    std::string test_logu{ log };
+                if (out_file.is_open()) {
+                    std::string test_logu{log};
                     out_file << test_logu << '\n';
-                }
-                else
-                {
-                    SCString log_error{ "Error writing into file" };
+                } else {
+                    SCString log_error{"Error writing into file"};
                     sc.AddMessageToLog(log_error, 1);
                 }
                 out_file.close();
@@ -113,9 +109,11 @@ SCSFExport scsf_ReadValuesFromPerzistVarsAndLogIntoTXT(SCStudyInterfaceRef sc)
             already_logged_perzist = 1;
         }
     }
-    if (PositionData.PositionQuantity == 0) { already_logged_perzist = 0; }	//reset the persist var for loging to zero again
+    if (PositionData.PositionQuantity == 0) {
+        already_logged_perzist = 0;
+    }  //reset the persist var for loging to zero again
 
-    //draw the data into chart																		
+    //draw the data into chart
     {
         s_UseTool tick;
         tick.Clear();
