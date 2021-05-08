28a260bb01cf64400703e1773269ea446052c614
lukas k <experimentalhypothesis@gmail.com>
2021-05-08 15:32:09 +0200

    add arrow marker to the bar where succesfull entry happened

--- MainTrendPattern.cpp
+++ MainTrendPattern.cpp
@@ -4,24 +4,16 @@
 
 SCDLLName("Nirvikalpa's Trend Patterns");
 
-
-// s_SCNewOrder& exitAtFixedTargetLoss(SCStudyInterfaceRef sc, s_SCNewOrder& order) {
-//     order.OrderQuantity = 1;
-//     order.OrderType = SCT_ORDERTYPE_MARKET;
-//     order.TextTag = "Three Candles Trend Pattern";
-//     order.Target1Offset = sc.Input[3].GetInt() * sc.TickSize;
-//     order.Stop1Offset = sc.Input[4].GetInt() * sc.TickSize;
-//     return order;
-// }
-
-
 SCSFExport scsf_TrendPatterns(SCStudyInterfaceRef sc) {
     SCInputRef inputStartTradingAt = sc.Input[0];
     SCInputRef inputStopTradingAt = sc.Input[1];
     SCInputRef inputFlatPostionAt = sc.Input[2];
-
     SCInputRef inputEmaPeriod = sc.Input[10];
+
     SCSubgraphRef subgraphEMA = sc.Subgraph[0];
+    SCSubgraphRef subgraphValidEntryBar = sc.Subgraph[1];
+    SCSubgraphRef subgraphBuyEntry = sc.Subgraph[10];
+    SCSubgraphRef subgraphSellEntry = sc.Subgraph[11];
 
     if (sc.SetDefaults) {
         sc.GraphName = "TrendPatterns";
@@ -40,9 +32,19 @@ SCSFExport scsf_TrendPatterns(SCStudyInterfaceRef sc) {
         sc.Input[4].Name = "Loss in ticks";
         sc.Input[4].SetInt(30);
 
-        sc.Subgraph[1].Name = "visible";
-        sc.Subgraph[1].DrawStyle = DRAWSTYLE_COLOR_BAR;
-        sc.Subgraph[1].PrimaryColor = RGB(0, 0, 255);
+        subgraphValidEntryBar.Name = "Bar Marker";
+        subgraphValidEntryBar.DrawStyle = DRAWSTYLE_COLOR_BAR;
+        subgraphValidEntryBar.PrimaryColor = RGB(0, 0, 255);
+
+        subgraphBuyEntry.Name = "Buy Entry";
+        subgraphBuyEntry.DrawStyle = DRAWSTYLE_ARROW_UP;
+        subgraphBuyEntry.PrimaryColor = RGB(255, 255, 255);
+        subgraphBuyEntry.LineWidth = 2;
+
+        subgraphSellEntry.Name = "Sell Entry";
+        subgraphSellEntry.DrawStyle = DRAWSTYLE_ARROW_DOWN;
+        subgraphSellEntry.PrimaryColor = RGB(255, 255, 255);
+        subgraphSellEntry.LineWidth = 2;
 
         subgraphEMA.Name = "Exp. Moving Average";
         subgraphEMA.DrawStyle = DRAWSTYLE_LINE;
@@ -50,8 +52,7 @@ SCSFExport scsf_TrendPatterns(SCStudyInterfaceRef sc) {
         inputEmaPeriod.Name = "Exp. Moving Average Period";
         inputEmaPeriod.SetInt(10);
 
-
-		sc.MaintainTradeStatisticsAndTradesData = true;
+        sc.MaintainTradeStatisticsAndTradesData = true;
 
         return;
     }
@@ -66,7 +67,7 @@ SCSFExport scsf_TrendPatterns(SCStudyInterfaceRef sc) {
 
     if (p_ThreeCandlesEMA == NULL)
         p_ThreeCandlesEMA = (aos::trend::ThreeCandlesEMA *)new aos::trend::ThreeCandlesEMA();
-   
+
     s_SCPositionData position;
     sc.GetTradePosition(position);
 
@@ -81,25 +82,32 @@ SCSFExport scsf_TrendPatterns(SCStudyInterfaceRef sc) {
     newOrder.OrderType = SCT_ORDERTYPE_MARKET;
     newOrder.TimeInForce = SCT_TIF_GOOD_TILL_CANCELED;
     newOrder.TextTag = "Three Candles Trend Pattern";
-    
+
     bool areTradingHours = sc.BaseDateTimeIn[sc.Index].GetTime() > inputStartTradingAt.GetTime() && sc.BaseDateTimeIn[sc.Index].GetTime() < inputStopTradingAt.GetTime();
     bool isTimeToFlat = sc.BaseDateTimeIn[sc.Index].GetTime() >= inputFlatPostionAt.GetTime();
     bool positionOpened = position.PositionQuantity != 0;
     bool priceCrossedEmaFromBellow = sc.CrossOver(sc.Close, subgraphEMA) == CROSS_FROM_TOP && sc.GetBarHasClosedStatus() == BHCS_BAR_HAS_CLOSED;
-     
-    
+
     if (areTradingHours) {
         if (p_ThreeCandlesEMA->isUp(sc)) {
+            subgraphValidEntryBar[sc.Index] = sc.Index;
             int entryCheck = (int)sc.BuyEntry(newOrder);
-            sc.Subgraph[1][sc.Index] = sc.Index;
-        }
-        else if (positionOpened && priceCrossedEmaFromBellow) {
+            if (entryCheck > 0) {
+                sc.AddMessageToLog(entryCheck, 1);
+                subgraphBuyEntry[sc.Index] = sc.Low[sc.Index];
+            }
+        } else if (positionOpened && priceCrossedEmaFromBellow) {
             (int)sc.BuyExit(newOrder);
+        } else if (p_ThreeCandlesEMA->isDown(sc)) {
+            subgraphValidEntryBar[sc.Index] = sc.Index;
+            int entryCheck = (int)sc.SellEntry(newOrder);
+            if (entryCheck > 0) {
+                sc.AddMessageToLog(entryCheck, 1);
+                subgraphSellEntry[sc.Index] = sc.High[sc.Index];
+            }
+        } else if (positionOpened && priceCrossedEmaFromBellow) {
+            (int)sc.SellExit(newOrder);
         }
-        // if (p_ThreeCandlesEMA->isDown(sc)) {
-        //     int entryCheck = (int)sc.SellEntry(order);
-        //     sc.Subgraph[1][sc.Index] = sc.Index;
-        // }
     }
 
     if (positionOpened && isTimeToFlat) sc.FlattenPosition();
